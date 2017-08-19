3c86df23dcf3fac42de506e17f493c7c80ebc13e
Yan Naing Win <yannaingwin@aceplussolutions.com>
2017-08-19 14:27:24 +0630

    added project

---
+++ app/src/main/java/com/example/user/selffix/CarReportTabletAdapter.java
@@ -0,0 +1,32 @@
+package com.example.user.selffix;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by user on 8/19/17.
+ */
+
+public class CarReportTabletAdapter extends RecyclerView.Adapter<CarReportTabletAdapter.MyViewHolder> {
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
